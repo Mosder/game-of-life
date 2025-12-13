39058783b75e6c22f491e1035d82859ed965f7e4
Mosder <xmosder12@gmail.com>
2025-12-13 16:05:13 +0000

    fixed mistakes in comments and cleaned up the code a tiny bit

--- bitmaps.c
+++ bitmaps.c
@@ -4,10 +4,11 @@
 
 // Initializes a bitmap of 0's with inputted width and height
 char* init_bitmap(int *width, int *height) {
-	printf("Width:\n");
+	printf("Width: ");
 	scanf("%d", width);
-	printf("Height:\n");
+	printf("Height: ");
 	scanf("%d", height);
+	printf("\n");
 
 	int width_bytes = (*width+7) / 8;
 	int size = *height * width_bytes;
@@ -34,26 +35,27 @@ void print_bitmap(char *bitmap, int width, int height) {
 			printf("%d", get_bit(bitmap, width_bytes, r, c));
 		printf("\n");
 	}
+	printf("\n");
 }
 
 // Shifts 2 bytes and returns OR of them
-// 	used for determining bytes representing neighboring bits
+// Used for determining bytes representing neighboring bits
 char shift_or_bytes(char byte1, int shift1, char byte2, int shift2) {
 	return (byte1 << shift1) | (byte2 >> shift2);
 }
 
 // Counts bits set to 1 across words on same positions
-// 	used for determining count of neighbors
+// Used for determining count of neighbors
 void count_bits_on_positions(int *counts, char *bytes) {
 	for (int pos = 0; pos < 8; pos++) {
 		int count = 0;
-		// counts from right to left
+		// count from right to left
 		for (int i = 0; i < 8; i++) {
-			count += bytes[i] & 1;
+			if (bytes[i] & 1) count++;
 			bytes[i] >>= 1;
 		}
 		// save from right to left, since it's counted from right to left
-		*(counts+7-pos) = count;
+		counts[7-pos] = count;
 	}
 }
 
@@ -62,7 +64,7 @@ int* count_neighbors_row(int width, char *row, char *row_above, char *row_below)
 	int width_bytes = (width+7) / 8;
 	int *counts = calloc(8*width_bytes, sizeof(int));
 
-	// array which contains neighbor data for the whole byte, one entry one neighbor data, 8 neighbors total
+	// array which contains neighbor data for the whole byte, one entry for one neighbor data, 8 neighbors total
 	char shifted_bytes[8];
 	// prev bytes to avoid handling first byte as a seperate case
 	char prev_byte_above = 0;
@@ -70,33 +72,33 @@ int* count_neighbors_row(int width, char *row, char *row_above, char *row_below)
 	char prev_byte_below = 0;
 
 	for (int byte = 0; byte < width_bytes; byte++) {
-		// top-left
+		// top-left neighbor
 		shifted_bytes[0] = shift_or_bytes(prev_byte_above, 7, *(row_above+byte), 1);
-		// center-left
+		// center-left neighbor
 		shifted_bytes[1] = shift_or_bytes(prev_byte, 7, *(row+byte), 1);
-		// bottom-left
+		// bottom-left neighbor
 		shifted_bytes[2] = shift_or_bytes(prev_byte_below, 7, *(row_below+byte), 1);
 
-		// top-center
+		// top-center neighbor
 		shifted_bytes[3] = *(row_above+byte);
-		// bottom-center
+		// bottom-center neighbor
 		shifted_bytes[4] = *(row_below+byte);
 
-		// determine if last byte, since no next byte and clipping needed for this byte
+		// determine if last byte - if so, don't consider next byte and clip the extra bits
 		if (byte != width_bytes-1) {
-			// top-right
+			// top-right neighbor
 			shifted_bytes[5] = shift_or_bytes(*(row_above+byte), 1, *(row_above+byte+1), 7);
-			// center-right
+			// center-right neighbor
 			shifted_bytes[6] = shift_or_bytes(*(row+byte), 1, *(row+byte+1), 7);
-			// bottom-right
+			// bottom-right neighbor
 			shifted_bytes[7] = shift_or_bytes(*(row_below+byte), 1, *(row_below+byte+1), 7);
 		}
 		else {
-			// top-right
+			// top-right neighbor
 			shifted_bytes[5] = *(row_above+width_bytes-1) << 1;
-			// center-right
+			// center-right neighbor
 			shifted_bytes[6] = *(row+width_bytes-1) << 1;
-			// bottom-right
+			// bottom-right neighbor
 			shifted_bytes[7] = *(row_below+width_bytes-1) << 1;
 
 			// clip extra bits
@@ -108,7 +110,7 @@ int* count_neighbors_row(int width, char *row, char *row_above, char *row_below)
 		}
 
 		// count neighbors for this byte
-		count_bits_on_positions(counts+8*byte, shifted_bytes);
+		count_bits_on_positions(counts + 8*byte, shifted_bytes);
 
 		// update prev bytes
 		prev_byte_above = *(row_above+byte);
@@ -128,13 +130,14 @@ char* decide_fate_row(char *row, int width, int *counts) {
 		char mask_2_neighbors = 0;
 		char mask_3_neighbors = 0;
 		for (int bit = 7; bit > -1; bit--) {
-			// access the bits from the least significant bit in a byte
+			// access the bits from the most significant bit in a byte
 			if (counts[8*byte + 7-bit] == 2)
 				mask_2_neighbors |= 1 << bit;
 			else if (counts[8*byte + 7-bit] == 3)
 	    			mask_3_neighbors |= 1 << bit;
 		}
-		// if 3 neighbors - always alive next frame and also stays alive if at least 2 neighbors
+		// if 3 neighbors - always alive next frame
+		// if 2 neighbors - stays alive
 		new_row[byte] = mask_3_neighbors | (mask_2_neighbors & row[byte]);
 	}
 	free(counts);
@@ -147,11 +150,11 @@ char* convolve_conway(char *bitmap, int width, int height) {
 	char *new_bitmap = calloc(height*width_bytes, sizeof(char));
 	char *zeros = calloc(width_bytes, sizeof(char));
 	
-	// prev row of zeros to not handle first row seperately
+	// prev row of zeros to avoid handling first row seperately
 	char *prev_row = zeros;
 	for (int r = 0; r < height; r++) {
 		int *counts;
-		// Determine if it's the last row, because not new row after last (duh)
+		// Determine if it's the last row, to set row below to zeros if so
 		if (r != height-1)
 			counts = count_neighbors_row(width, bitmap + r*width_bytes, prev_row, bitmap + (r+1)*width_bytes);
 		else
@@ -173,15 +176,15 @@ char* convolve_conway(char *bitmap, int width, int height) {
 int main() {
 	int width, height;
 	char *bitmap = init_bitmap(&width, &height);
+
 	set_byte(bitmap, (width+7)/8, 1, 0, 0b00111000);
 	set_byte(bitmap, (width+7)/8, 3, 0, 0b00110000);
 	set_byte(bitmap, (width+7)/8, 4, 0, 0b00110000);
 	print_bitmap(bitmap, width, height);
-	bitmap = convolve_conway(bitmap, width, height);
-	printf("\n");
-	print_bitmap(bitmap, width, height);
-	bitmap = convolve_conway(bitmap, width, height);
-	printf("\n");
-	print_bitmap(bitmap, width, height);
+
+	for (int i = 0; i < 5; i++) {
+		bitmap = convolve_conway(bitmap, width, height);
+		print_bitmap(bitmap, width, height);
+	}
 }
 
